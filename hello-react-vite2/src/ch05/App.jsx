85d3b6cfa6860adc3180f833882bb21a62c3ece6
정성규 <jsg213213@naver.com>
2026-05-02 10:36:12 +0900

    260502

--- hello-react-vite2/src/ch05/App.jsx
+++ hello-react-vite2/src/ch05/App.jsx
@@ -8,11 +8,11 @@ class App extends Component {
     return (
       <div>
         <h1>ch05 연습용</h1>
-        <h2>연습1, 간단한 유효성 체크 확인.</h2>
+        <h2>간단한 유효성 체크 확인.</h2>
         <ValidationSample></ValidationSample>
-        <h2>연습2, 앞시간에 회원가입폼에서, 특정 입력창에 포커스 주기 복습</h2>
+        <h2>회원가입폼에서, 특정 입력창에 포커스 주기 복습</h2>
         <JoinForm></JoinForm>
-        <h2>연습3, 스크롤에 ref 달기, 제어 연습</h2>
+        <h2>스크롤에 ref 달기, 제어 연습</h2>
         {/* 순서3, 부모 컴포넌트에서, 자식 컴포넌트 ref로 전달하기. */}
         <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
         {/* 버튼 클릭시, 순서3으로 이동  */}
