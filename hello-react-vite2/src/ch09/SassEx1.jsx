f1aad81cc900e6c2014b03d1ce3f0fb227bd5535
정성규 <jsg213213@naver.com>
2026-05-06 19:52:47 +0900

    260506

--- hello-react-vite2/src/ch09/SassEx1.jsx
+++ hello-react-vite2/src/ch09/SassEx1.jsx
@@ -7,7 +7,7 @@ const SassEx1 = () => {
 
       <div className="card">
         <h2 className="title">카드에 중첩으로 요소들 적용해보기 </h2>
-        <p>오늘의 점심메뉴:감자탕</p>
+        <p>오늘의 점심메뉴:소불고기돌솥비빔밥</p>
       </div>
 
       <div>
