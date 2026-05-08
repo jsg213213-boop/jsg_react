0781d8c4fa3d6b7a9c5d368036caed48fba5a600
정성규 <jsg213213@naver.com>
2026-05-08 15:30:30 +0845

    260508

---
+++ hello-react-vite2/src/ch12/App.jsx
@@ -0,0 +1,28 @@
+import Ex1 from './ex/Ex1';
+import ImmerTest from './ImmerTest';
+import Ex2 from './ex/Ex2';
+import ImmerTest2 from './ImmerTest2';
+import Ex3 from './ex/Ex3';
+import Ex4 from './ex/Ex4';
+
+const App = () => {
+  return (
+    <div>
+      <h1>immer 라이브러리</h1>
+      <h2>실습1</h2>
+      <Ex1></Ex1>
+      <h2>예시1</h2>
+      <ImmerTest></ImmerTest>
+      <h2>실습2</h2>
+      <Ex2></Ex2>
+      <h2>예시2</h2>
+      <ImmerTest2></ImmerTest2>
+      <h2>실습3</h2>
+      <Ex3></Ex3>
+      <h2>실습4</h2>
+      <Ex4></Ex4>
+    </div>
+  );
+};
+
+export default App;
