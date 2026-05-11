c95a14155543c25697132d296ae405fde8b4ac6d
정성규 <jsg213213@naver.com>
2026-05-11 11:13:12 +0845

    260511

--- hello-react-vite2/src/main.jsx
+++ hello-react-vite2/src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
-import App from './ch12/App';
+import App from './ch13/App';
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>
 )
