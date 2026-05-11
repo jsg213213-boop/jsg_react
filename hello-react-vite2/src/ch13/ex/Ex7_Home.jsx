6f22340b166fae9534e93ac63c7ae17bb15fa5e2
정성규 <jsg213213@naver.com>
2026-05-11 19:58:08 +0900

    260511

--- hello-react-vite2/src/ch13/ex/Ex7_Home.jsx
+++ hello-react-vite2/src/ch13/ex/Ex7_Home.jsx
@@ -6,7 +6,7 @@ const Ex7_Home = () => {
       <h1>실습 7 홈 페이지 🏠</h1>
       <p>환영합니다! 여기는 메인 홈 화면입니다.</p>
       <li>
-        <Link to="/posts/lsy">정성규 프로필</Link>
+        <Link to="/posts/jsg">정성규 프로필</Link>
       </li>
     </div>
   );
