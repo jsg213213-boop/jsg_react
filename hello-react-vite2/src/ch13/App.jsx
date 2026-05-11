35fcd3495a79bddc5567e1f99038cf9e7166425d
정성규 <jsg213213@naver.com>
2026-05-11 17:16:28 +0845

    260511

--- hello-react-vite2/src/ch13/App.jsx
+++ hello-react-vite2/src/ch13/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, NavLink } from 'react-router-dom'; // NavLink 추가
+import {Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Ex1 from './ex/Ex1';
@@ -8,70 +8,47 @@ import Ex3 from './ex/Ex3';
 import Articles from './pages/Articles';
 import Article from './pages/Article';
 import Ex4 from './ex/Ex4';
-import Ex5 from './ex/Ex5'; // Layout 역할
+import Ex5 from './ex/Ex5';
 import UseNavigate from './pages/UseNavigate';
 import NotFound from './pages/NotFound';
 import MyPage from './pages/MyPage';
 import Login from './pages/Login';
 import Ex6 from './ex/Ex6';
 
-// 추가되는 컴포넌트 (파일 분리 권장이나 실습을 위해 여기에 작성)
-import Posts from './pages/Posts'; 
-import PostDetail from './pages/PostDetail';
-
 const App = () => {
-  // NavLink 스타일 설정
-  const activeStyle = ({ isActive }) => ({
-    fontWeight: isActive ? "bold" : "normal",
-    color: isActive ? "royalblue" : "gray",
-    textDecoration: "none",
-    marginRight: "15px"
-  });
-
   return (
     <div>
       <h1>ch13 리액트 라우팅 연습</h1>
       <h2>"react-router-dom": "^7.15.0"</h2>
-
-      {/* 1. 요구사항: 헤더에 홈/게시판 NavLink 적용 */}
-      <nav style={{ padding: '10px', borderBottom: '2px solid #eee' }}>
-        <NavLink to="/" style={activeStyle}>홈</NavLink>
-        <NavLink to="/posts" style={activeStyle}>게시판</NavLink>
-        <NavLink to="/about" style={activeStyle}>소개</NavLink>
-        <NavLink to="/articles" style={activeStyle}>기사목록</NavLink>
-      </nav>
-      
+      {/* 내비게이션 메뉴 */}
+      {/* <nav>
+        <Link to="/">홈</Link> | <Link to="/about">소개</Link>|{' '}
+        <Link to="/contact">연락처-Ex1</Link> |{' '}
+        <Link to="/about2">소개-Ex3</Link> |{' '}
+        <Link to="/articles">Articles</Link>
+      </nav> */}
       <hr />
-
+      {/* 라우트 설정 */}
       <Routes>
-        {/* 공통 레이아웃 Ex5로 감싸기 */}
+        {/* Layout 안에 묶인 라우트들은 Layout을 공유 */}
         <Route element={<Ex5 />}>
-          <Route path="/" element={<Home />} /> {/* 홈: "미니 블로그에 오신 것을 환영합니다"가 Home에 있어야 함 */}
-          
-          {/* 2. 요구사항: 게시판 중첩 라우팅 */}
-          <Route path="/posts" element={<Posts />}>
-            <Route path=":id" element={<PostDetail />} />
-          </Route>
-
-          {/* 기존 라우트들 */}
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Ex1 />} />
           <Route path="/profiles/:username" element={<Profile />} />
           <Route path="/profiles2/:username" element={<Ex2 />} />
           <Route path="/about2" element={<Ex3 />} />
           <Route path="/articles" element={<Articles />}>
-            <Route index element={<Ex4 />} />
+            <Route index element={<Ex4 />} /> {/*기본 자식 컴포넌트로 활용 */}
             <Route path=":id" element={<Article />} />
           </Route>
           <Route path="/useNavigate" element={<UseNavigate />} />
           <Route path="/myPage" element={<MyPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/myPage2" element={<Ex6 />} />
-          <Route path="/posts/:id" element={<Posts />} />
-          <Route path="/postsDetail/:id" element={<PostDetail />} />
-
-          {/* 3. 요구사항: 404 페이지 */}
-          <Route path="*" element={<NotFound />} />
+          {/* App.jsx Routes 마지막에 추가 */}
+          <Route path="*" element={<NotFound />} />{' '}
+          {/* * = 어떤 경로도 매칭 안될 때 */}
         </Route>
       </Routes>
     </div>
