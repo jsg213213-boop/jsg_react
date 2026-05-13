be1b281c0842511b55ab7dc1a854ce4aed517e75
정성규 <jsg213213@naver.com>
2026-05-13 09:37:52 +0845

    260513

--- hello-react-vite2/src/ch14/ex/Ex6.jsx
+++ hello-react-vite2/src/ch14/ex/Ex6.jsx
@@ -1,12 +1,6 @@
 import usePromise from '../newsApi/usePromise';
 import axios from 'axios';
 
-//### 📝 실습 문제 6
-
-// > `usePromise` 훅을 사용해서
-// `https://jsonplaceholder.typicode.com/users` API를 호출하는 컴포넌트를 만드세요.
-// 사용자 이름(name)과 이메일(email) 목록을 화면에 표시하세요.
-// >
 const Ex6 = () => {
   const [loading, resolved, error] = usePromise(
     () => axios.get('https://jsonplaceholder.typicode.com/users'),
