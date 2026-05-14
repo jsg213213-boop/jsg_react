9932ad87c42dcde22c236c7aa24e7724a7f3bda2
정성규 <jsg213213@naver.com>
2026-05-14 17:16:02 +0845

    260514

--- hello-react-vite2/src/main.jsx
+++ hello-react-vite2/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import './index.css'
-import App from './ch15/App';
+import App from './ch14/App';
 //import Ex7_App from "./ch13/ex/Ex7_App";
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
